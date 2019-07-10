b38c71e006239b91c3dc72138f75d8ee09e91c85
Nicholas Pung <nicholaspung>
2019-07-10 14:59:41 -0700

    Finished MVP

--- AnimalKingdom/src/animalKingdom/Main.java
+++ AnimalKingdom/src/animalKingdom/Main.java
@@ -16,6 +16,7 @@ public class Main
     }
     public static void main(String[] args)
     {
+        // Initiating objects
         Mammal m1 = new Mammal("Panda", 1869);
         Mammal m2 = new Mammal("Zebra", 1778);
         Mammal m3 = new Mammal("Koala", 1816);
@@ -34,11 +35,13 @@ public class Main
         Fish f2 = new Fish("Catfish", 1817);
         Fish f3 = new Fish("Perch", 1758);
 
-        System.out.println(f1.getId());
-        System.out.println(b1.getName());
-        System.out.println(m1.getYear());
-        System.out.println();
+        // Testing AbstractAnimal methods
+        // System.out.println(f1.getId());
+        // System.out.println(b1.getName());
+        // System.out.println(m1.getYear());
+        // System.out.println();
 
+        // Adding objects to an ArrayList
         ArrayList<AbstractAnimal> animalList = new ArrayList<AbstractAnimal>();
         animalList.add(m1);
         animalList.add(m2);
@@ -58,6 +61,7 @@ public class Main
         animalList.add(f2);
         animalList.add(f3);
 
+        // Printing out all animals to make sure it works
         System.out.println("List of all animals");
         for (AbstractAnimal a : animalList)
         {
@@ -65,16 +69,19 @@ public class Main
         }
         System.out.println();
 
+        // o1 - o2 is ascending order, o2 - o1 is descending order
         System.out.println("Animals sorted in descending order by year named");
         animalList.sort((o1, o2) -> o2.getYear() - o1.getYear());
         printAnimals(animalList, a -> a instanceof AbstractAnimal);
         System.out.println();
 
+        // o1.compareToIgnoreCase(o2) is a-z, o2.compareToIgnoreCase(o1) is z-a
         System.out.println("Animals sorted alphabetically");
         animalList.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
         printAnimals(animalList, a -> a instanceof AbstractAnimal);
         System.out.println();
 
+        // Using a checker method and lambda expressions. Using printAnimals to follow DRY
         System.out.println("Animals sorted by how they move");
         animalList.sort((o1, o2) -> o1.move().compareToIgnoreCase(o2.move()));
         printAnimals(animalList, a -> a instanceof AbstractAnimal);
@@ -92,6 +99,7 @@ public class Main
         printAnimals(animalList, a -> (a.reproduce() == "eggs") && (a.breath() == "lungs"));
         System.out.println();
 
+        // Sorting the list alphabetically first, then using lambda expression to output sort
         System.out.println("Animals sorted alphabetically named in 1758");
         animalList.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
         printAnimals(animalList, a -> a.getYear() == 1758);
